e54b2f59cabeba163b198d3b94881457ee4fe2e2
Luis de Haro <luisdeharo4@gmail.com>
2024-01-27 11:46:28 +0100

    refactor search api

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -7,24 +7,22 @@ export class WeatherService {
 
     public urlWeatherApi: string = 'https://weatherapi-com.p.rapidapi.com/current.json?q=';
 
-    constructor() {
+    constructor() { }
 
-    }
-
-
-    //Refactorizar
-    async searchWeatherCity(nameCity: string) {
-        const options = {
+    get _headersMethod(): Object {
+        return {
             method: 'GET',
             headers: {
                 'X-RapidAPI-Key': 'ca8883ebacmsh7f08012256b1c56p1407a3jsn9b1bc30c6e8a',
                 'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
             }
         };
-
+    }
+    //Refactorizar
+    async searchWeatherCity(nameCity: string) {
         try {
             const urlWeatherApiNameCity = this.urlWeatherApi + nameCity;
-            const res = await fetch(urlWeatherApiNameCity, options);
+            const res = await fetch(urlWeatherApiNameCity, this._headersMethod);
             const result = await res.json();
             console.log(result.current);
         } catch (error) {
